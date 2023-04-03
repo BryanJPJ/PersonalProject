555c027e0173e6f06a0444137ab34deb7a3bc59a
Bryan <bryanpjpgi2b@hotmail.com>
2023-04-03 17:46:49 +0200

    first commit

---
+++ src/test/java/personalproject/demo/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package personalproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
